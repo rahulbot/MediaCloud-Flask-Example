4fbdac8c4d91db5a07e77b9554dd6629eb557039
Rahul B <rahulbot@gmail.com>
2017-11-21 09:52:17 -0500

    linting

--- mcserver.py
+++ mcserver.py
@@ -21,18 +21,22 @@ mc = mediacloud.api.MediaCloud( config.get('mediacloud','api_key') )
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("search-form.html")
+
 
 @app.route("/search",methods=['POST'])
 def search_results():
     keywords = request.form['keywords']
     now = datetime.datetime.now()
     results = mc.sentenceCount(keywords,
-        solr_filter=[mc.publish_date_query( datetime.date( 2017, 1, 1), now ), 'tags_id_media:9139487' ])
-    return render_template("search-results.html", 
-        keywords=keywords, sentenceCount=results['count'] )
+                               solr_filter=[mc.publish_date_query( datetime.date( 2017, 1, 1), now ),
+                                            'tags_id_media:9139487'])
+    return render_template("search-results.html",
+                           keywords=keywords,
+                           sentenceCount=results['count'] )
 
 if __name__ == "__main__":
     app.debug = True
